d8b797786ee122a8d51051195db42328165b6e70
josephitopa <talktoitopa@gmail.com>
2026-05-25 21:55:09 +0100

    storing terraform state on s3

---
+++ day-08/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform_bucket_250526"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "terraform-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "s"
+    }
+}
+
+terraform {
+    bucket "s3" {
+        bucket = "terraform_bucket_250526"
+        key = "global/s3/terraform.tfstate"
+        region = "us-east-2"
+        dynamodb_table = "terraform_locks"
+        encrypt = true
+    }
+}
